1b147863689a12acdf8fb28ea5ef361b1ab85282
ChrystianOrren <orren.chris@gmail.com>
2024-04-09 10:44:16 -0400

    Fixed navbar links from shop page

---
+++ zenapp/static/js/navbar.js
@@ -0,0 +1,48 @@
+// Load redirect value from localStorage on script initialization
+let redirect = localStorage.getItem('redirect') || '';
+
+function diffPageLink(event) {
+    var page_title = document.title;
+    var href = event.target.getAttribute("href");
+    if (page_title == "Dashboard") {
+        console.log("Dashboard");
+    } else {
+        if (href && href.startsWith("#")) {
+            console.log("Redirecting to Dashboard: ", href);
+            var return_path = "/" + href;
+            redirect = return_path;
+            localStorage.setItem('redirect', redirect); // Save redirect value to localStorage
+            window.location.href = return_path;
+        } else {
+            return;
+        }
+    }
+}
+
+function toggleMenu(event) {
+    var ham_menu = document.getElementById("hamburger-menu");
+    ham_menu.classList.toggle("show");
+    if (event) {
+        diffPageLink(event);
+    }
+}
+
+function timedRedirect() {
+    if (redirect != "") {
+        window.location.href = redirect;
+        redirect = "";
+        localStorage.removeItem('redirect')
+    }
+}
+
+// Fix initial element shift
+window.onload = function() {
+    var page_title = document.title;
+    if (page_title == "Dashboard") {
+        var homeLink = document.querySelector('.navbar a[href="#home"]');
+        if (homeLink) {
+            homeLink.click();
+            timedRedirect();
+        }
+    }
+};
