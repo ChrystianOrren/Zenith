974905d2f8d1f95ee1ed42efaff115823f35dc65
ChrystianOrren0 <orren.chris@gmail.com>
2024-04-15 16:27:18 -0400

    shop mostly done

--- zenapp/static/js/shop.js
+++ zenapp/static/js/shop.js
@@ -1,3 +1,39 @@
-function clickCard() {
-    console.log("hello");
+function clickCard(imgUrl, shaper, num, desc) {
+    console.log(imgUrl, shaper, num, desc);
+    var grid = document.getElementById("grid");
+    var popUp = document.getElementById("popUp");
+    if (grid && popUp){
+        grid.style.pointerEvents = "none";
+        grid.style.filter = "blur(3px)";
+        popUp.style.display = "flex";
+
+        var img = document.getElementById("popUpImg");
+        img.src = imgUrl
+        var shaperNum = document.getElementById("shaperNum");
+        shaperNum.textContent = shaper + ' - ' + num;
+        var descElement = document.getElementById("description");
+        descElement.textContent = desc;
+    }
+}
+
+function purchaseClick() {
+    var purchaseBtn = document.getElementById("purchaseBtn");
+    var purchaseInfo = document.getElementById("purchaseInfo");
+    purchaseBtn.style.display = "none";
+    purchaseInfo.style.display = "flex"
+    purchaseInfo.style.flexDirection ="column";
+}
+
+function closePopUp() {
+    var grid = document.getElementById("grid");
+    var popUp = document.getElementById("popUp");
+    var purchaseBtn = document.getElementById("purchaseBtn");
+    var purchaseInfo = document.getElementById("purchaseInfo");
+    if (grid && popUp){
+        grid.style.pointerEvents = "auto";
+        grid.style.filter = "none";
+        popUp.style.display = "none"
+        purchaseBtn.style.display = "inline-block";
+        purchaseInfo.style.display = "none"
+    }
 }
