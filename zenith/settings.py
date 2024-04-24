528d61b5db8e0f4ae11f16d265d82dfae2a940b7
ChrystianOrren0 <orren.chris@gmail.com>
2024-04-24 19:51:22 -0400

    ah shit here we go again

--- zenith/settings.py
+++ zenith/settings.py
@@ -16,6 +16,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
