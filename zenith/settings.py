8bdf7e6453efa60f4a1f52fe881c78c0b82cd229
ChrystianOrren0 <orren.chris@gmail.com>
2024-04-24 19:18:43 -0400

    again with this settings stuff

--- zenith/settings.py
+++ zenith/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/home/ubuntu/zenith/zenapp/static'
+STATICFILES_DIRS = [
+    '/home/ubuntu/zenith/zenapp/static/',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
